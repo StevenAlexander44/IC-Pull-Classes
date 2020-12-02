b601b9ebddb08842f3409692fe0e6dc3e55bc170
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2020-12-01 20:20:46 -0500

    Added Terms Support

--- UseThisIfYouGoToManual.js
+++ UseThisIfYouGoToManual.js
@@ -1,23 +1,24 @@
 async function main () {
-	const domain = 'https://jcpsky.infinitecampus.org/campus/resources/portal/'
-	const classes = await fetch(domain + 'roster?_enableScheduleForGrades=true', {headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}})
+	const classes = await fetch(
+		'/campus/resources/portal/roster?_enableScheduleForGrades=true',
+		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}})
 		.then(r => r.json())
 	for (const period of classes) {
-		const {
-			periods: [{ name }],
-			periods: [{ periodSchedule }],
-			periods: [{ periodID }]
-		} = await fetch(domain + `section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`, {headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}})
-			.then(r => r.json())
-		delete period.sessionID
+		const {periods: [{ periodID }], terms: [{ termName }], periods: [{ name }], periods: [{ periodSchedule }]} = await fetch(
+			`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,
+			{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}})
+		.then(r => r.json())
+		delete period.sectionID
 		period.period = name
 		period.block = periodSchedule.name
 		period.sort = periodID
+		period.term = termName
 	}
 	return classes
-		.sort((a, b) => +a.sort - +b.sort)
-		.map(({ block, period, teacherDisplay, courseName }) => `${block} ${period}: ${teacherDisplay} / ${courseName}`)
-		.join('\n')
+	.sort((a, b) => +a.sort - +b.sort)
+	.sort((a, b) => +a.term - +b.term)
+	.map(({ block, period, teacherDisplay, courseName }) => `${block} ${period}: ${teacherDisplay} / ${courseName}`)
+	.join('\n')
 }
 
 main()
