8f36bf5470708558b285114d100383522f497220
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2020-12-01 19:59:45 -0500

    Update CopyPasteIntoConsole.js

--- CopyPasteIntoConsole.js
+++ CopyPasteIntoConsole.js
@@ -1,5 +1,3 @@
-// document.head.appendChild(document.createElement('script')).src = 'https://raw.githubusercontent.com/StevenAlexander44/IC-Pull-Classes/master/CopyPasteIntoConsole.js'
-
 async function main () {
 	const classes = await fetch(
 		'https://jcpsky.infinitecampus.org/campus/resources/portal/roster?_enableScheduleForGrades=true', 
