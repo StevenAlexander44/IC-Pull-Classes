9432a4c567690922e8d38e86c878da57fa3287b4
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2020-12-01 20:22:47 -0500

    Update CopyPasteIntoConsole.js

--- CopyPasteIntoConsole.js
+++ CopyPasteIntoConsole.js
@@ -1,18 +1,16 @@
 async function main () {
 	const classes = await fetch(
-		'https://jcpsky.infinitecampus.org/campus/resources/portal/roster?_enableScheduleForGrades=true', 
-		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}}
-	)
-	.then(r => r.json())
+		'/campus/resources/portal/roster?_enableScheduleForGrades=true', 
+		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}})
+		.then(r => r.json())
 	for (const period of classes) {
 		const {periods: [{ periodID }], terms: [{ termName }]} = await fetch(
-			`https://jcpsky.infinitecampus.org/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,
-			{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}}
-		)
+			`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,
+			{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}})
 		.then(r => r.json())
 		delete period.sectionID
 		period.period = periodID
-        period.term = termName
+        	period.term = termName
 	}
 	return classes
 	.sort((a, b) => +a.period - +b.period)
