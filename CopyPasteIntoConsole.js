184871f56b0524567ca7ba3e580cde2c62b2a723
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-06-12 01:02:12 -0400

    Update CopyPasteIntoConsole.js

--- CopyPasteIntoConsole.js
+++ CopyPasteIntoConsole.js
@@ -1,25 +1,20 @@
-async function main () {
-	const classes = await fetch(
-		'/campus/resources/portal/roster?_enableScheduleForGrades=true', 
-		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}})
-		.then(r => r.json())
-	for (const period of classes) {
-		const {periods: [{ periodID }], terms: [{ termName }]} = await fetch(
-			`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,
-			{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}})
-		.then(r => r.json())
-		delete period.sectionID
-		period.id = periodID
-		period.term = termName
-	}
-	return classes
+const classes = await fetch(
+	'/campus/resources/portal/roster?_enableScheduleForGrades=true', 
+	{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}}
+).then(r => r.json())
+for (const period of classes) {
+	const {periods: [{ periodID }], terms: [{ termName }]} = await fetch(
+		`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,
+		{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}}
+	).then(r => r.json())
+	period.id = periodID
+	period.term = termName
+}
+output = classes
 	.sort((a, b) => +a.id - +b.id)
 	.sort((a, b) => +a.term - +b.term)
 	.map(({ term, id, courseName, teacherDisplay }) => `Term ${term} | PeriodID ${id}: ${teacherDisplay} / ${courseName}`)
 	.join('\n')
-}
-main()
-	.then(output => {
-		console.log(output)
-		alert(output)
-	})
+console.log(output)
+alert(output)
+document.body.innerText = output
