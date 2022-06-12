a42cb4e2d946aaf33e839200521c1a262e33e2f9
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-06-12 00:32:43 -0400

    Update UseThisIfYouGoToManual.js

--- UseThisIfYouGoToManual.js
+++ UseThisIfYouGoToManual.js
@@ -1,14 +1,15 @@
 async function main () {
 	const classes = await fetch(
 		'/campus/resources/portal/roster?_enableScheduleForGrades=true',
-		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}})
-		.then(r => r.json())
+		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}}
+	)
+	.then(r => r.json())
 	for (const period of classes) {
 		const {periods: [{ periodID }], terms: [{ termName }], periods: [{ name }], periods: [{ periodSchedule }]} = await fetch(
 			`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,
-			{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}})
+			{headers: {'Accept': 'application/json, text/plain, */*','Cache-Control': 'no-cache'}}
+		)
 		.then(r => r.json())
-		delete period.sectionID
 		period.id = periodID
 		period.term = termName
 		period.day = periodSchedule.name
@@ -20,8 +21,8 @@ async function main () {
 	.map(({ term, day, number, teacherDisplay, courseName }) => `Term ${term} | ${day} ${number}: ${teacherDisplay} / ${courseName}`)
 	.join('\n')
 }
-main()
-	.then(output => {
-		console.log(output)
-		alert(output)
-	})
+main().then(output => {
+	console.log(output)
+	alert(output)
+	document.body.innerText = output
+})
