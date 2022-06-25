1eb9e8cf1799a08eef93b17154dbe49554027fee
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-06-25 04:50:05 -0400

    Update UseThisIfYouGoToManual.js

--- UseThisIfYouGoToManual.js
+++ UseThisIfYouGoToManual.js
@@ -1,29 +1,28 @@
-const classes = await fetch("/campus/resources/portal/roster?_enableScheduleForGrades=true", {
-	{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}}
-}).then(a => a.json());
-for (const period of classes) {
-	const { periods: [{ periodID }], terms: [{ termName }], periods: [{ name }], periods: [{ periodSchedule }] } = await fetch(`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`, {
-		{headers: {'Accept': 'application/json, text/plain, */*', 'Cache-Control': 'no-cache'}}
-	).then(r => r.json())
-	period.id = periodID
-	period.term = termName
-	period.day = periodSchedule.name
-	period.number = name
-}
-text = classes.sort((a, b) => +a.id - +b.id).sort((a, b) => +a.term - +b.term)
-arr = text.map(({ term, day, number, teacherDisplay, courseName }) => ["Term: " + term, day + number, teacherDisplay, courseName])
-text = text.map(({ term, day, number, teacherDisplay, courseName }) => `Term ${term} | ${day} ${number}: ${teacherDisplay} / ${courseName}`).join("\n")
-console.log(text)
-alert(text)
-var table = "<table>"
-for (const clas of arr) {
-	table += "<tr>"
-	for (const thing of clas) {
-		table += `<td>${thing}</td>`
-	}
-	table += "</tr>"
-}
-table += "</table>"
-document.body.innerHTML = table
-document.head.innerHTML = ""
-document.title = "Schedule"
+javascript:(()=>{
+	!async function() {
+		const classes = await fetch("/campus/resources/portal/roster?_enableScheduleForGrades=true",{
+			headers:{Accept:"application/json, text/plain, */*","Cache-Control":"no-cache"}
+		}).then(e=>e.json());
+		for (const period of classes) {
+			const {periods:[{periodID}],terms:[{termName}],periods:[{name}],periods:[{periodSchedule}]} = await fetch(
+				`/campus/resources/portal/section/${period.sectionID}?_expand=course-school&_expand=terms&_expand=periods-periodSchedule&_expand=teacherPreference&_expand=room&_expand=teachers`,{
+					headers:{Accept:"application/json, text/plain, */*","Cache-Control":"no-cache"}
+				}).then(e=>e.json());
+			period.id = periodID,period.term=termName,period.day=periodSchedule.name,period.number=name
+		}
+		output = classes.sort((a, b) => +a.id - +b.id).sort((a, b) => +a.term - +b.term)
+		arrput = output.map(({ term, day, number, teacherDisplay, courseName }) => ["Term: " + term, day + number, teacherDisplay, courseName])
+		output = output.map(({ term, day, number, teacherDisplay, courseName }) => `Term ${term} | ${day} ${number}: ${teacherDisplay} / ${courseName}`).join("\n")
+		console.log(output), alert(output);
+		var table="<table>";
+		for (const clas of arrput) {
+			table+="<tr>";
+			for (const o of clas) {
+				table += `<td>${o}</td>`
+			}
+			table += "</tr>"
+		}
+		table += "</table>"
+		document.body.innerHTML=table,document.head.innerHTML="",document.title="Schedule"
+	}()
+})();
